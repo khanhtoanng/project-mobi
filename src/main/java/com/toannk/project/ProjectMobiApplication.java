01e8656ef719d99c86a4c169d4d3ba6dd3884d3f
toannknknk <nguyenkhanhtoan812@gmail.com>
2020-07-06 07:42:43 +0700

    add code

---
+++ src/main/java/com/toannk/project/ProjectMobiApplication.java
@@ -0,0 +1,13 @@
+package com.toannk.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMobiApplication.class, args);
+	}
+
+}
